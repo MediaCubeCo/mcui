0c6ef3f6a8476587d5398e26c6a3c75b679b1fd2
kirillidze <krs@mediacube.network>
2020-07-21 15:08:40 +0300

    global styles in mc-root

--- src/templates/layouts/McRoot/McRoot.stories.js
+++ src/templates/layouts/McRoot/McRoot.stories.js
@@ -10,5 +10,22 @@ export default {
 // default state
 export const Default = () => ({
   components: { McRoot },
-  template: `<mc-root>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dolorem eligendi eum illum quibusdam! Doloremque dolores in molestiae nesciunt veritatis!</mc-root>`,
+  template: `<mc-root>
+      <h1 class="mb-s">Global classes</h1>
+
+      <h2 class="mb-s">Spaces:</h2>
+      <p>margin-bottom - mb-#{value}</p>
+      <p>margin-top - mt-#{value}</p>
+      <p>padding-bottom - pb-#{value}</p>
+      <p>padding-bottom - pb-#{value}</p>
+
+      <h2 class="mb-s">Values:</h2>
+      <p>Same as sizes tokens</p>
+
+      <h2 class="mb-s">Visibilities:</h2>
+      <p>hidden-m (>768)</p>
+      <p>hidden-l (>1024)</p>
+      <p>hidden-m-down (<768)</p>
+      <p>hidden-l-down (<1024)</p>
+  </mc-root>`,
 });
