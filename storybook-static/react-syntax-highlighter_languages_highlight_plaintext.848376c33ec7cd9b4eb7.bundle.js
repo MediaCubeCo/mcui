c2bd3f41968709c029f917c87ac909f0af35601a
kirillidze <krs@mediacube.network>
2020-09-25 11:30:08 +0300

    delete folders

--- storybook-static/react-syntax-highlighter_languages_highlight_plaintext.848376c33ec7cd9b4eb7.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[121],{2330:function(module,exports){module.exports=function(hljs){return{disableAutodetect:!0}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_plaintext.848376c33ec7cd9b4eb7.bundle.js.map
