fc79c2b9629517bf42668a4ee6bb709c9404ad16
Deniska <dsa@mediacube.network>
2021-03-09 12:15:37 +0300

    Add reverseHeaders prop to mc-progress

--- src/elements/McProgress/McProgress.vue
+++ src/elements/McProgress/McProgress.vue
@@ -1,128 +1,136 @@
 <script>
 export default {
-  functional: true,
-  name: "McProgress",
-  props: {
-    /**
-     *  Процент
-     *
-     */
-    percent: {
-      type: Number,
-      default: 0,
+    name: 'McProgress',
+    functional: true,
+    props: {
+        /**
+         *  Процент
+         *
+         */
+        percent: {
+            type: Number,
+            default: 0,
+        },
+        /**
+         *  Цвет
+         *
+         */
+        color: {
+            type: String,
+            default: 'dark-gray',
+        },
+        /**
+         *  Вспомогательный текст
+         */
+        helpText: {
+            type: String,
+            default: '',
+        },
+        /**
+         *  Ошибки
+         */
+        errors: {
+            type: Array,
+            default: () => [],
+        },
+        /**
+         *  Поменять местами подписи
+         */
+        reverseHeaders: {
+            type: Boolean,
+            default: false,
+        },
     },
-    /**
-     *  Цвет
-     *
-     */
-    color: {
-      type: String,
-      default: "dark-gray",
+    render(h, { props, data }) {
+        const errorText = props.errors !== null && props.errors.length ? props.errors.join(', ') : ''
+        const wrapperClasses = {
+            'mc-progress': true,
+            [`mc-progress--color-${props.color}`]: props.color,
+            'mc-progress--error': errorText,
+            ...(data.class || {}),
+        }
+
+        if (data.staticClass) {
+            const staticClasses = data.staticClass.split(' ')
+            staticClasses.forEach(c => c && (wrapperClasses[c] = true))
+        }
+
+        let style = {}
+        if (data.staticStyle) {
+            style = data.staticStyle
+        }
+
+        const lineClasses = {
+            'mc-progress__line': true,
+        }
+
+        return h(
+            'div',
+            {
+                class: wrapperClasses,
+                style,
+            },
+            [
+                h(
+                    'div',
+                    {
+                        class: ['mc-progress__top', { 'mc-progress__top--reverse-headers': this.reverseHeaders }],
+                    },
+                    [
+                        h(
+                            'div',
+                            {
+                                class: 'mc-progress__percent',
+                            },
+                            [`${errorText ? 0 : props.percent}%`],
+                        ),
+                        props.helpText
+                            ? h(
+                                  'div',
+                                  {
+                                      class: 'mc-progress__help-text',
+                                  },
+                                  [props.helpText],
+                              )
+                            : null,
+                    ],
+                ),
+                h(
+                    'div',
+                    {
+                        class: 'mc-progress__wrapper-line',
+                    },
+                    [
+                        h('div', {
+                            class: lineClasses,
+                            style: { width: `${errorText ? 2 : props.percent}%` },
+                        }),
+                    ],
+                ),
+                errorText
+                    ? h(
+                          'div',
+                          {
+                              class: 'mc-progress__errors-container',
+                          },
+                          [errorText],
+                      )
+                    : null,
+            ],
+        )
     },
-    /**
-     *  Вспомогательный текст
-     */
-    helpText: {
-      type: String,
-      default: "",
-    },
-    /**
-     *  Ошибки
-     */
-    errors: {
-      type: Array,
-      default: () => [],
-    },
-  },
-  render(h, { props, data }) {
-    const errorText = (props.errors !== null && props.errors.length) ? props.errors.join(", ") : ""
-    const wrapperClasses = {
-      'mc-progress': true,
-      [`mc-progress--color-${props.color}`]: props.color,
-      'mc-progress--error': errorText,
-      ...(data.class || {}),
-    }
-
-    if (data.staticClass) {
-      const staticClasses = data.staticClass.split(" ")
-      staticClasses.forEach(c => c && (wrapperClasses[c] = true))
-    }
-
-    let style = {}
-    if (data.staticStyle) {
-      style = data.staticStyle
-    }
-
-    const lineClasses = {
-      'mc-progress__line': true,
-    }
-
-    return h(
-      'div',
-      {
-        class: wrapperClasses,
-        style,
-      },
-      [
-        h(
-          "div",
-          {
-            class: 'mc-progress__top',
-          },
-          [
-            h(
-              "div",
-              {
-                class: 'mc-progress__percent',
-              },
-              [`${errorText ? 0 : props.percent}%`],
-            ),
-            props.helpText ? h(
-              "div",
-              {
-                class: 'mc-progress__help-text',
-              },
-              [props.helpText],
-            ) : null,
-          ]
-        ),
-        h(
-          "div",
-          {
-            class: 'mc-progress__wrapper-line',
-          },
-          [
-            h(
-              "div",
-              {
-                class: lineClasses,
-                style: { width: `${errorText ? 2 : props.percent}%` },
-              },
-            ),
-          ]
-        ),
-        errorText ? h(
-          "div",
-          {
-            class: 'mc-progress__errors-container',
-          },
-          [errorText]
-        ) : null,
-      ]
-    )
-  },
 }
 </script>
 
 <style lang="scss">
 .mc-progress {
-  $block-name: &;
+    $block-name: &;
 
-  position: relative;
-  width: 100%;
-  font-family: $font-family-main;
-  line-height: $line-height-200;
-  font-size: $font-size-200;
+    position: relative;
+    width: 100%;
+    font-family: $font-family-main;
+    line-height: $line-height-200;
+    font-size: $font-size-200;
 
     @each $color, $value in $token-colors {
         &--color-#{$color} {
@@ -153,6 +161,9 @@ export default {
         align-items: center;
         justify-content: space-between;
         margin-bottom: $space-100;
+        &--reverse-headers {
+            flex-direction: row-reverse;
+        }
     }
 
     &__help-text {
@@ -166,11 +177,11 @@ export default {
         width: 100%;
     }
 
-  &__line {
-    height: 100%;
-    border-radius: $radius-100;
-    max-width: 100%;
-  }
+    &__line {
+        height: 100%;
+        border-radius: $radius-100;
+        max-width: 100%;
+    }
 
     &__errors-container {
         margin-top: $space-150;
