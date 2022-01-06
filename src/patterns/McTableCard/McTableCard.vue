1e38e638844f702790b10c0909c41e3c718c2ffd
Deniska <dsa@mediacube.network>
2022-01-06 18:45:33 +0300

    Add blur to McTableCard footer slot

--- src/patterns/McTableCard/McTableCard.vue
+++ src/patterns/McTableCard/McTableCard.vue
@@ -1,68 +1,120 @@
 <template>
-  <div class="mc-table-card" :style="{ marginLeft: ml }">
-    <div v-if="$slots.header" class="mc-table-card__header">
-      <!-- @slot Слот заголовка -->
-      <slot name="header" />
+    <div class="mc-table-card" :style="{ marginLeft: ml }">
+        <div v-if="$slots.header" class="mc-table-card__header">
+            <!-- @slot Слот заголовка -->
+            <slot name="header" />
+        </div>
+        <div class="mc-table-card__body">
+            <!-- @slot Слот контента -->
+            <slot />
+        </div>
+        <div v-if="$slots.footer" class="mc-table-card__footer" :class="computedFooterClasses">
+            <!-- @slot Слот футера -->
+            <slot name="footer" />
+        </div>
     </div>
-    <div class="mc-table-card__body">
-      <!-- @slot Слот контента -->
-      <slot />
-    </div>
-    <div v-if="$slots.footer" class="mc-table-card__footer">
-      <!-- @slot Слот футера -->
-      <slot name="footer" />
-    </div>
-  </div>
 </template>
 
 <script>
-
 export default {
-  name: "McTableCard",
-  props: {
-    ml: {
-      type: String,
-      default: "251px",
+    name: 'McTableCard',
+    props: {
+        ml: {
+            type: String,
+            default: '251px',
+        },
+        footerBlur: {
+            type: Boolean,
+            default: false,
+        },
     },
-  },
+    data() {
+        return {
+            bodyEl: null,
+            body_scrolled_to_bottom: true,
+        }
+    },
+    computed: {
+        computedFooterClasses() {
+            return {
+                'mc-table-card__footer--with-blur': this.footerBlur && !this.body_scrolled_to_bottom,
+            }
+        },
+    },
+    mounted() {
+        if (this.footerBlur) {
+            try {
+                this.body_scrolled_to_bottom = false
+                this.bodyEl = document.querySelector('.mc-table-card__body')
+                this.bodyEl && this.bodyEl.addEventListener('scroll', this.handlerScroll)
+            } catch (e) {
+                console.error(e)
+            }
+        }
+    },
+    beforeDestroy() {
+        this.bodyEl && this.bodyEl.removeEventListener('scroll', this.handlerScroll)
+    },
+    methods: {
+        handlerScroll(e) {
+            try {
+                this.body_scrolled_to_bottom = e.target.offsetHeight + e.target.scrollTop === e.target.scrollHeight
+            } catch (e) {
+                this.body_scrolled_to_bottom = true
+                console.error(e)
+            }
+        },
+    },
 }
 </script>
 
 <style lang="scss">
 .mc-table-card {
-  $block-name: &;
+    $block-name: &;
 
-  @include position(absolute, 0);
-  z-index: 10;
-  background-color: $color-white;
-  border-left: 1px solid $color-hover-gray;
-  display: flex;
-  flex-direction: column;
-  @include custom-scroll();
-
-  &__header {
-    height: $size-500;
-    border-bottom: 1px solid $color-hover-gray;
-    flex-shrink: 0;
-  }
-
-  &__body {
-    position: relative;
-    flex-grow: 1;
+    @include position(absolute, 0);
+    z-index: 10;
+    background-color: $color-white;
+    border-left: 1px solid $color-hover-gray;
     display: flex;
     flex-direction: column;
-    min-height: 0;
-    overflow-y: auto;
-  }
+    @include custom-scroll();
 
-  &__footer {
-    border-top: 1px solid $color-hover-gray;
-    flex-shrink: 0;
-    padding: $space-200;
-    @include child-indent-right($space-150);
-    &:empty {
-      display: none;
+    &__header {
+        height: $size-500;
+        border-bottom: 1px solid $color-hover-gray;
+        flex-shrink: 0;
     }
-  }
+
+    &__body {
+        position: relative;
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+        min-height: 0;
+        overflow-y: auto;
+    }
+
+    &__footer {
+        border-top: 1px solid $color-hover-gray;
+        flex-shrink: 0;
+        padding: $space-200;
+        @include child-indent-right($space-150);
+        &:empty {
+            display: none;
+        }
+        &--with-blur {
+            position: relative;
+            &::after {
+                @include pseudo;
+                height: $space-900;
+                background: linear-gradient(to top, $color-white, transparent);
+                position: absolute;
+                width: 100%;
+                left: 0;
+                bottom: 100%;
+            }
+        }
+    }
 }
 </style>
