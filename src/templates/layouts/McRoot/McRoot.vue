152b017e7d6fb223c5152734ccc18b74920a993e
Deniska <dsa@mediacube.network>
2022-01-06 14:59:09 +0300

    Adaptive McRoot for ssr, and add props for exclude panel

--- src/templates/layouts/McRoot/McRoot.vue
+++ src/templates/layouts/McRoot/McRoot.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="mc-root">
-    <slot />
-    <slideout-panel :class="panelClass"></slideout-panel>
-  </div>
+    <div class="mc-root">
+        <slot />
+        <client-only v-if="ssr && includePanel">
+            <slideout-panel :class="panelClass"></slideout-panel>
+        </client-only>
+        <slideout-panel v-else-if="includePanel" :class="panelClass"></slideout-panel>
+    </div>
 </template>
 
 <script>
@@ -12,22 +15,30 @@ import 'normalize.css/normalize.css'
  * т.к. здесь находятся глобальные стили.
  */
 export default {
-  name: 'McRoot',
-  props: {
-    panelClass: {
-      type: String,
-      default: '',
+    name: 'McRoot',
+    props: {
+        panelClass: {
+            type: String,
+            default: '',
+        },
+        ssr: {
+            type: Boolean,
+            default: false,
+        },
+        includePanel: {
+            type: Boolean,
+            default: true,
+        },
     },
-  },
 }
 </script>
 
 <style lang="scss">
 .mc-root {
-  $block-name: &;
-  @import '../../../styles/global';
-  .slideout {
-    box-shadow: $shadow-s;
-  }
+    $block-name: &;
+    @import '../../../styles/global';
+    .slideout {
+        box-shadow: $shadow-s;
+    }
 }
 </style>
