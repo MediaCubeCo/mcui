0792b03f3e085f762f7a05a79d9897a22fee9687
Deniska <dsa@mediacube.network>
2022-02-25 19:36:27 +0300

    Add new size for McPreview

--- src/patterns/McPreview/McPreview.vue
+++ src/patterns/McPreview/McPreview.vue
@@ -1,119 +1,129 @@
 <template>
-  <section class="mc-preview" :class="classes">
-    <div v-if="$slots.left" class="mc-preview__left">
-      <!-- @slot Слот слева -->
-      <slot name="left" />
-    </div>
-    <div class="mc-preview__center">
-      <div class="mc-preview__top">
-        <!-- @slot Слот сверху -->
-        <slot name="top" />
-      </div>
-      <div class="mc-preview__bottom">
-        <!-- @slot Слот внизу -->
-        <slot name="bottom" />
-      </div>
-    </div>
-    <div v-if="$slots.right" class="mc-preview__right">
-      <!-- @slot Слот справа -->
-      <slot name="right" />
-    </div>
-  </section>
+    <section class="mc-preview" :class="classes">
+        <div v-if="$slots.left" class="mc-preview__left">
+            <!-- @slot Слот слева -->
+            <slot name="left" />
+        </div>
+        <div class="mc-preview__center">
+            <div class="mc-preview__top">
+                <!-- @slot Слот сверху -->
+                <slot name="top" />
+            </div>
+            <div class="mc-preview__bottom">
+                <!-- @slot Слот внизу -->
+                <slot name="bottom" />
+            </div>
+        </div>
+        <div v-if="$slots.right" class="mc-preview__right">
+            <!-- @slot Слот справа -->
+            <slot name="right" />
+        </div>
+    </section>
 </template>
 
 <script>
 export default {
-  name: "McPreview",
-  props: {
-    /**
-     *  Размеры:
-     *  `s, m, l`
-     */
-    size: {
-      type: String,
-      default: "m",
+    name: 'McPreview',
+    props: {
+        /**
+         *  Размеры:
+         *  `s, m, l`
+         */
+        size: {
+            type: String,
+            default: 'm',
+        },
     },
-  },
-  computed: {
-    classes() {
-      return {
-        [`mc-preview--size-${this.size}`]: this.size,
-      }
+    computed: {
+        classes() {
+            return {
+                [`mc-preview--size-${this.size}`]: this.size,
+            }
+        },
     },
-  }
 }
 </script>
 
 <style lang="scss">
 .mc-preview {
-  $block-name: &;
+    $block-name: &;
 
-  @include reset-text-indents();
-  display: flex;
-  flex-wrap: nowrap;
-  max-width: 100%;
+    @include reset-text-indents();
+    display: flex;
+    flex-wrap: nowrap;
+    max-width: 100%;
 
-  &__left {
-    flex: 0 0 auto;
-    &:empty {
-      display: none;
+    &__left {
+        flex: 0 0 auto;
+        &:empty {
+            display: none;
+        }
     }
-  }
 
-  &__center {
-    @include layout-flex-fix();
-    flex: 1 1 auto;
-    align-self: center;
-  }
+    &__center {
+        @include layout-flex-fix();
+        flex: 1 1 auto;
+        align-self: center;
+    }
 
-  &__top {
-    &:empty {
-      display: none;
+    &__top {
+        &:empty {
+            display: none;
+        }
     }
-  }
 
-  &__bottom {
-    &:empty {
-      display: none;
+    &__bottom {
+        &:empty {
+            display: none;
+        }
     }
-  }
 
-  &__right {
-    flex: 0 0 auto;
-    &:empty {
-      display: none;
+    &__right {
+        flex: 0 0 auto;
+        &:empty {
+            display: none;
+        }
     }
-  }
 
-  &--size {
-    &-s {
-      #{$block-name}__left {
-        margin-right: $space-50;
-      }
-      #{$block-name}__right {
-        margin-left: $space-50;
-      }
+    &--size {
+        &-s {
+            #{$block-name}__left {
+                margin-right: $space-50;
+            }
+            #{$block-name}__right {
+                margin-left: $space-50;
+            }
+        }
+        &-m {
+            #{$block-name}__left {
+                margin-right: $space-100;
+            }
+            #{$block-name}__right {
+                margin-left: $space-100;
+            }
+        }
+        &-l {
+            #{$block-name}__left {
+                margin-right: $space-150;
+            }
+            #{$block-name}__right {
+                margin-left: $space-150;
+            }
+            #{$block-name}__bottom {
+                margin-top: $space-50;
+            }
+        }
+        &-xl {
+            #{$block-name}__left {
+                margin-right: $space-200;
+            }
+            #{$block-name}__right {
+                margin-left: $space-200;
+            }
+            #{$block-name}__bottom {
+                margin-top: $space-50;
+            }
+        }
     }
-    &-m {
-      #{$block-name}__left {
-        margin-right: $space-100;
-      }
-      #{$block-name}__right {
-        margin-left: $space-100;
-      }
-    }
-    &-l {
-      #{$block-name}__left {
-        margin-right: $space-150;
-      }
-      #{$block-name}__right {
-        margin-left: $space-150;
-      }
-      #{$block-name}__bottom {
-        margin-top: $space-50;
-      }
-    }
-  }
-
 }
 </style>
