63975dce58aa71c5d7258ebe54c42f314e034d68
Alexander <am@mediacube.io>
2023-09-05 11:10:45 +0300

    CU-862k705j3 - Добавить сторис для mediacube-ui с отображением всех иконок добавленных в ДС

--- src/elements/McSvgIcon/McSvgIcon.stories.js
+++ src/elements/McSvgIcon/McSvgIcon.stories.js
@@ -2,11 +2,13 @@ import { text, select, number } from '@storybook/addon-knobs'
 
 import McSvgIcon from './McSvgIcon'
 import McSeparator from '../McSeparator/McSeparator.vue'
+import McFieldText from '../McField/McFieldText/McFieldText.vue'
 import McTitle from '../McTitle/McTitle.vue'
 import { getTokensByType, getTokenGroup } from '../../utils/getTokens'
 import { SVG_ICONS } from '../../helpers/storybookVariables'
 
 export default {
+    components: {McFieldText},
     title: 'Elements/McSvgIcon',
     component: McSvgIcon,
     parameters: {
@@ -52,7 +54,11 @@ export const IconsList = () => ({
         McSvgIcon,
         McTitle,
         McSeparator,
+        McFieldText,
     },
+    data: () => ({
+        search: null,
+    }),
     computed: {
         iconsSections() {
             const iconsObject = {
@@ -76,31 +82,44 @@ export const IconsList = () => ({
                     default: return iconsObject.other.push(icon)
                 }
             })
-            return Object.keys(iconsObject).map(key => ({
+            const defaultSections = Object.keys(iconsObject).map(key => ({
                 title: key.replace(/^./, key[0].toUpperCase()),
                 icons: iconsObject[key],
                 id: key,
             }))
+            return this.search
+                ? [{
+                    title: 'Search results',
+                    icons: SVG_ICONS.filter(icon => icon.includes(this.search))
+                }]
+                : defaultSections
         },
     },
     template: `
         <section>
+            <mc-field-text v-model="search" name="search" title="Search" placeholder="add" class="mb-400" />
             <div v-for="section in iconsSections" :key="section.id" :id="section.id">
                 <mc-title variation="h3">
                     {{ section.title }}
                 </mc-title>
-                <mc-separator indent-bottom="400" indent-top="400" />
-                <div style="display: grid; grid-template-columns: repeat(auto-fit, 160px); gap: 16px">
-                    <div
-                        v-for="icon in section.icons"
-                        :key="icon"
-                        style="display: flex; flex-direction: column; align-items: center; justify-content: center"
-                    >
-                        <mc-svg-icon :name="icon" size="400" />
-                        <mc-title :ellipsis="false" text-align="center" class="mt-150">{{ icon }}</mc-title>
+                <template v-if="section.icons.length">
+                    <mc-separator indent-bottom="400" indent-top="400" />
+                    <div style="display: grid; grid-template-columns: repeat(auto-fit, 160px); gap: 16px">
+                        <div
+                            v-for="icon in section.icons"
+                            :key="icon"
+                            style="display: flex; flex-direction: column; align-items: center; justify-content: center"
+                        >
+                            <mc-svg-icon :name="icon" size="400" />
+                            <mc-title :ellipsis="false" text-align="center" class="mt-150">{{ icon }}</mc-title>
+                        </div>
                     </div>
+                    <mc-separator indent-bottom="400" indent-top="400" />
+                </template>
+                <div v-else style="width: 100%; height: 300px; display: flex; align-items: center; justify-content: center; flex-direction: column">
+                    <mc-svg-icon name="info" color="gray" size="800" />
+                    <mc-title text-align="center" color="gray" variation="h3">No results</mc-title>
                 </div>
-                <mc-separator indent-bottom="400" indent-top="400" />
             </div>
         </section>
     `,
